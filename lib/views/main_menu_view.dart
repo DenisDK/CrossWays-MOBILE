19f1b811381cec6fd42ecc76f513d9df520f93e1
AlexStavok <oleksandr.stavytskyi@nure.ua>
2024-10-27 00:01:28 +0300

    mainMenuDesign

--- lib/views/main_menu_view.dart
+++ lib/views/main_menu_view.dart
@@ -10,6 +10,105 @@ class MainMenuView extends StatefulWidget {
 class _MainMenuViewState extends State<MainMenuView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'CrossWays 🛩️',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 135, 100, 71),
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.person,
+                        color: Color.fromARGB(255, 135, 100, 71),
+                        size: 28,
+                      ),
+                      Icon(
+                        Icons.more_vert,
+                        color: Color.fromARGB(255, 135, 100, 71),
+                        size: 28,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 20)),
+            Center(
+              child: Container(
+                width: 370,
+                height: 750,
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage('assets/beachPhoto.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Stack(
+                  children: [
+                    Center(
+                      child: Column(
+                        children: [
+                          Padding(padding: EdgeInsets.only(top: 240)),
+                          const Text(
+                            'Welcome back!',
+                            style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            shadows: [
+                              Shadow(
+                                offset: Offset(2, 2),
+                                blurRadius: 5,
+                                color: Color.fromARGB(255, 92, 109, 103)
+                              )
+                            ]
+                          ),
+                            textAlign: TextAlign.center,
+                          ),
+                          Padding(padding: EdgeInsets.only(top: 25)),
+                          SizedBox(
+                            width: 230,
+                            height: 30,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                // Дія при натисканні кнопки
+                              },
+                              style: ElevatedButton.styleFrom(
+                                elevation: 9,
+                                shadowColor: Color.fromARGB(255, 92, 109, 103),
+                              ),
+                              child: Text(
+                                'Create a new trip',
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 92, 109, 103),
+                                ),
+                              ),
+                            ),
+                          )
+
+                        ],
+                      )
+                    )
+                  ]
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
